f00989e9862259c0ea41e940a0eb8bd290389c73
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-21 00:01:27 -0300

    feat: Added propTypes and setting as required

--- src/components/SeriesList.js
+++ src/components/SeriesList.js
@@ -1,9 +1,16 @@
-import React from "react"
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const SeriesList = props => {
+  const { series } = props
+
   return (
     <h1>Hi, there!</h1>
   )
+}
+
+SeriesList.propTypes = {
+  series: PropTypes.object.isRequired
 }
 
 export default SeriesList
