0aa000bf52bca254487882114b92fe26ea5f9e91
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-21 02:09:14 -0300

    feat: Component for displaying the list of each series

--- src/components/SeriesList.js
+++ src/components/SeriesList.js
@@ -1,13 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { Link } from 'react-router-dom'
+import {
+  Card,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardColumns,
+  CardBody,
+  Button
+} from 'reactstrap'
+ 
 const SeriesList = props => {
   const { series } = props
 
+  const serieDetails = title => {
+    return title.toLowerCase()
+    .replace(/ /g,'-')
+    .replace(/[^\w-]+/g,'')
+  }
+
   return (
-    <h1>Hi, there!</h1>
-  )
-}
+    <CardColumns>
+      {series && series.map(serie => (
+        <Card key={serie.id}>
+          <CardImg
+            top
+            width="100%"
+            src={`${serie.thumbnail.path}.${serie.thumbnail.extension}`}
+            alt={`${serie.title}`}
+          />
+          <CardBody>
+            <CardTitle>
+              {serie.title}
+            </CardTitle>
+            <CardText>
+              {serie.description || 'No description.'}
+            </CardText>
+            <Link to={serieDetails(serie.title)}>
+              <Button>Details</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      ))}
+     </CardColumns>
+   )
+ }
 
 SeriesList.propTypes = {
   series: PropTypes.array.isRequired
