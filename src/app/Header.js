55a3f0270a24106e68be0a5806fe70a385168b47
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-20 21:57:18 -0300

    feat: Added header component

---
+++ src/app/Header.js
@@ -0,0 +1,40 @@
+import React from "react"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from "reactstrap"
+
+
+const Header = props => {
+  return (
+    <div className="header">
+      <Navbar
+        color="light" 
+        light
+        expand="md"
+      >
+        <NavbarBrand href="/">Marvel Series</NavbarBrand>
+        <NavbarToggler />
+        <Collapse navbar>
+          <Nav
+            className="ml-auto"
+            navbar
+          >
+            <NavItem>
+              <NavLink href="/series">
+                Series
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
