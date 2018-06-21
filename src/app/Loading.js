69512dbcfe5c555d98ccc3dbcbaf7f1bc8c2e9cf
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-20 23:45:39 -0300

    feat: Center loading in the screen

--- src/app/Loading.js
+++ src/app/Loading.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Spinner from 'react-spinkit'
 
+const style = {
+  position: 'fixed',
+  zIndex: 999,
+  height: '2em',
+  width: '2em',
+  overFlow: 'show',
+  margin: 'auto',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0
+}
+
 const Loading = () => {
   return (
-    <Spinner name="pacman" />
+    <Spinner style={style} name="pacman" />
   )
 }
 
