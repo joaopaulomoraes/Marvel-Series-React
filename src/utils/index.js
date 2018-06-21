5d1365caa46f56b9382eb5f229bb4392688bb1f9
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-21 12:57:21 -0300

    docs: Added description to funcion functionality

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,13 @@
 import md5 from 'md5'
 import credentials from '../config'
 
+/* Create new timestamp */
 export const timestamp = +Date.now()
 
-export const generateHash = () => {
+/**
+ * @description Generate new md5 hashes for API requests
+ * @returns A hash param to using in API call
+ */
+const generateHash = () => {
   return md5( timestamp + credentials.privateKey + credentials.publicKey )
 }
