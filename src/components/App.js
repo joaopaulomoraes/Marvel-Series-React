1e87d94b30400362db34666765412d23d57f2f2c
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-20 22:19:21 -0300

    feat: Added new components and route configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom"
+import Header from '../app/Header'
+import SeriesList from './SeriesList'
+import SerieDetails from './SerieDetails'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Welcome to Marvel Series API</h1>
+        <Header />
+        <Router>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/" component={SeriesList} />
+              <Route exact path="/series/:title" component={SerieDetails} />
+              <Route path="/series" component={SeriesList} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     )
   }
