f4a01e62c1c0681e5f9734ae39268a9561925fe7
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-21 12:58:06 -0300

    fix: Export expression added

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,6 +8,6 @@ export const timestamp = +Date.now()
  * @description Generate new md5 hashes for API requests
  * @returns A hash param to using in API call
  */
-const generateHash = () => {
+export const generateHash = () => {
   return md5( timestamp + credentials.privateKey + credentials.publicKey )
 }
