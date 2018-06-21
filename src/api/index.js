c77079384a04ff0dd38e8448b93bc700e2180d98
João Paulo Moraes <joaopaulomoraes@outlook.com>
2018-06-21 13:04:02 -0300

    docs: Added necessary documentation

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,11 @@ import {
   generateHash
 } from '../utils'
 
+/**
+ * @description Create default instance to API calls
+ * @return New Promise instance
+ * @memberof MarvelAPI
+ */
 const MarvelAPI = axios.create({
   baseURL: 'https://gateway.marvel.com/v1/public/',
   params: {
@@ -14,10 +19,21 @@ const MarvelAPI = axios.create({
   }
 })
 
+/**
+ * @description Make the call in the endpoint series
+ * @return New Promise instance for series
+ * @memberof MarvelAPI
+ */
 export const getSeries = () => {
   return MarvelAPI.get(`/series`)
 }
 
+/**
+ * @description Make the call in the endpoint series/:id
+ * @param id Serie id
+ * @return New Promise instance for series based in Id
+ * @memberof MarvelAPI
+ */
 export const getSerie = id => {
   return MarvelAPI.get(`/series/${id}`)
 }
